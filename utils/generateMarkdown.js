e258b7b3b2188ed401c1b34faa3e681b658fdf27
bryce <blarkin13@gmail.com>
2022-02-25 18:12:22 -0600

    file structure

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,52 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+const generateMarkdown = data => {
+  return `
+  <h1 align="center">${data.projectName}<h1/>
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ![badmath](https://img.shields.io/npm/l/open)
+  This project is licensed under ${data.licenses}
+  
+  ## Credits
+  ${data.credits}
+
+  ## Contributing
+  ${data.contributers}
+
+  ## Questions
+  > GitHub username: ${data.username}
+  > If you have additional questions about this project, you can contact me at ${data.email}. 
+
+`;
+};
+
+
+module.exports = generateMarkdown;
